12c70acd7ebf47293afae92357fabad7049b79ba
Edwin Nel <nel.edwin@gmail.com>
2025-04-10 06:00:00 +0000

    luno exchange

---
+++ freqtrade/exchange/luno.py
@@ -0,0 +1,22 @@
+import logging
+
+from freqtrade.exchange import Exchange
+from freqtrade.exchange.exchange_types import FtHas
+
+
+logger = logging.getLogger(__name__)
+
+
+class Luno(Exchange):
+    """
+    Luno exchange class. Contains adjustments needed for Freqtrade to work
+    with this exchange.
+
+    Please note that this exchange is not included in the list of exchanges
+    officially supported by the Freqtrade development team. So some features
+    may still not work as expected.
+    """
+
+    _ft_has: FtHas = {
+        "ohlcv_partial_candle": False,
+    }
